43b9fd84865f15df8ab9404770f4ef4d351ee215
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-03-30 13:30:48 +0530

    getMember , createMember view added

--- wemeet/views.py
+++ wemeet/views.py
@@ -1,9 +1,16 @@
+from calendar import c
 import imp
+from unicodedata import name
 from django.shortcuts import render
 from django.http import JsonResponse
 from agora_token_builder import RtcTokenBuilder
 import random
 import time
+import json
+
+from .models import RoomMember
+
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -29,3 +36,27 @@ def getToken(request):
     token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
     return JsonResponse({'token' : token, 'uid':uid}, safe = False)
 
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+    
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid = data['UID'],
+        room_name = data['room_name']
+    )
+    return JsonResponse({'name' : data['name']}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get('UID')
+    room_name  = request.GET.get('room_name')
+    
+    member = RoomMember.objects.get(
+        uid = uid,
+        room_name = room_name,
+        
+    )
+    name = member.name
+    return JsonResponse({'name':member.name}, safe=False)
