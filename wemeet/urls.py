6629394beb360a6e21a04656eed6c918a7b60169
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-03-30 13:31:45 +0530

    create_member, get_member url path added

--- wemeet/urls.py
+++ wemeet/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.lobby, name='lobby'),
     path('room/', views.room , name='room'),
     path('get_token/', views.getToken),
+    
+    path('create_member/' , views.createMember),
+    path('get_member/' , views.getMember),
+    
 
 ]
